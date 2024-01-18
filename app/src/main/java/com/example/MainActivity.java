372f15e4c1896a1017ea2dbec95f69faf0a1c680
lyzao <136966940+lyzao@users.noreply.github.com>
2024-01-18 18:24:01 +0100

    projet AAA, M2 MIASHS

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean termsAccepted = false;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         afficherTermesUtilisation();
 
         // Récupération du Switch pour le mode sombre
-        Switch switchModeSombre = findViewById(R.id.switch1);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch switchModeSombre = findViewById(R.id.switch1);
 
         switchModeSombre.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -37,14 +43,106 @@ public class MainActivity extends AppCompatActivity {
                     // Mode sombre activé : changer le fond en noir et le texte en blanc
                     findViewById(R.id.mainLayout).setBackgroundColor(Color.BLACK);
                     ((TextView) findViewById(R.id.textview2)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.temp)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.hum)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.pre)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.gaz)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.textView5)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.textView6)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.textView7)).setTextColor(Color.WHITE);
+                    ((TextView) findViewById(R.id.textView8)).setTextColor(Color.WHITE);
+
+                    ImageView imageView = findViewById(R.id.imageView8); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView.setImageResource(R.drawable.thermometerb);
+
+                    ImageView imageView1 = findViewById(R.id.imageView3); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView1.setImageResource(R.drawable.humidityw);
+
+                    ImageView imageView2 = findViewById(R.id.imageView4); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView2.setImageResource(R.drawable.barometerw);
+
+                    ImageView imageView3 = findViewById(R.id.imageView5); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView3.setImageResource(R.drawable.gasw);
+
+
                     switchModeSombre.setTextColor(Color.WHITE);
                     // Modifier les couleurs pour d'autres éléments si nécessaire
                 } else {
                     // Mode sombre désactivé : revenir aux couleurs par défaut
                     findViewById(R.id.mainLayout).setBackgroundColor(Color.WHITE);
                     ((TextView) findViewById(R.id.textview2)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.temp)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.hum)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.pre)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.gaz)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.textView5)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.textView6)).setTextColor(Color.BLACK);
+                        ((TextView) findViewById(R.id.textView7)).setTextColor(Color.BLACK);
+                    ((TextView) findViewById(R.id.textView8)).setTextColor(Color.BLACK);
+                    ImageView imageView = findViewById(R.id.imageView8); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView.setImageResource(R.drawable.thermometerw);
+
+                    ImageView imageView1 = findViewById(R.id.imageView3); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView1.setImageResource(R.drawable.humidityb);
+
+                    ImageView imageView2 = findViewById(R.id.imageView4); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView2.setImageResource(R.drawable.barometerb);
+
+                    ImageView imageView3 = findViewById(R.id.imageView5); // Remplacez mon_imageview par l'ID de votre ImageView
+                    imageView3.setImageResource(R.drawable.gasb);
                     switchModeSombre.setTextColor(Color.BLACK);
+
+
+
                     // Revenir aux couleurs par défaut pour d'autres éléments si nécessaire
+                }
+            }
+        });
+
+        // Initialiser TextToSpeech
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    // Définir la langue en français
+                    int result = textToSpeech.setLanguage(Locale.FRANCE);
+
+                    // Vérifier si la langue est supportée
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        // Gérer le cas où la langue n'est pas disponible ou n'est pas prise en charge
+                        Toast.makeText(MainActivity.this, "La langue française n'est pas disponible.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // Gérer le cas d'échec de l'initialisation de TextToSpeech
+                    Toast.makeText(MainActivity.this, "Échec de l'initialisation de la synthèse vocale.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        // Ajouter un écouteur de clic pour le bouton de lecture
+        Button buttonLire = findViewById(R.id.buttonLire);
+        buttonLire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (termsAccepted) {
+                    // Obtenez le texte que vous souhaitez lire
+                    TextView tempTextView = findViewById(R.id.temp);
+                    TextView humTextView = findViewById(R.id.hum);
+                    TextView preTextView = findViewById(R.id.pre);
+                    TextView gazTextView = findViewById(R.id.gaz);
+                    // Obtenez le texte du TextView
+                    String temperature = tempTextView.getText().toString();
+                    String humidity = humTextView.getText().toString();
+                    String pression = preTextView.getText().toString();
+                    String gaz = gazTextView.getText().toString();
+
+                    // Concaténez la chaîne "La température est " avec la température
+                    String texteALire = "La température est   " + temperature + "l'humidity est à  " + humidity+ "La pression est à   " + pression + "le gaz est à   " + gaz ;
+
+                    // Utilisez TextToSpeech pour lire le texte
+                    textToSpeech.speak(texteALire, TextToSpeech.QUEUE_FLUSH, null, null);
+                } else {
+                    // Affichez un message demandant à l'utilisateur d'accepter les termes
+                    Toast.makeText(MainActivity.this, "Veuillez accepter les termes d'utilisation", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -53,7 +151,17 @@ public class MainActivity extends AppCompatActivity {
     private void afficherTermesUtilisation() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Conditions d'utilisation de l'application AirQuality")
-                .setMessage("ici je vais mettre les termes d'utilisation (l'application utilisera les informations d'air de votre pièce)  ")
+                .setMessage("Objectif :\n" +
+                        "Fournir des informations sur la qualité de l'air de votre pièce.\n" +
+                        "\n" +
+                        "Collecte de données :\n" +
+                        "Vos données sont anonymes, utilisées uniquement pour améliorer nos services.\n" +
+                        "\n" +
+                        "Mises à jour :\n" +
+                        "Nous pouvons mettre à jour l'application pour améliorer ses fonctionnalités.\n" +
+                        "\n" +
+                        " Support :\n" +
+                        "Pour toute question contactez-nous à AirQualityapp@email.com")
                 .setPositiveButton("Accepter", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // Action lorsque l'utilisateur accepte les termes
@@ -74,22 +182,31 @@ public class MainActivity extends AppCompatActivity {
                                         finish();
                                     }
                                 })
-                                .setNegativeButton("Accepter les termes", null) // Annuler la fermeture de l'application
+                                .setNegativeButton("Accepter les termes", new DialogInterface.OnClickListener() {
+                                 @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                // Action lorsque l'utilisateur accepte les termes
+                                        termsAccepted = true; // Indiquer que les termes ont été acceptés
+                                // Continuer avec la fermeture de la boîte de dialogue de confirmation de sortie
+                                        dialog.dismiss();
+                                     }
+                                 })
                                 .show();
-
                     }
                 })
                 .setCancelable(false) // Empêcher de fermer la boîte de dialogue en cliquant à l'extérieur
                 .show();
     }
 
-    public void openSecondPage(View view) {
-        if (termsAccepted) { // Vérifier si les termes ont été acceptés avant d'ouvrir la deuxième page
-            Intent intent = new Intent(this, SecondActivity.class);
-            startActivity(intent);
-        } else {
-            // Afficher un message pour indiquer à l'utilisateur d'accepter les termes
-            // Vous pouvez afficher un Toast ou une boîte de dialogue pour informer l'utilisateur ici
+    // ... (le reste de votre code)
+
+    @Override
+    protected void onDestroy() {
+        // Libérez les ressources de TextToSpeech lors de la fermeture de l'activité
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
         }
+        super.onDestroy();
     }
 }
